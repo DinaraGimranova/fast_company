937c21153715fdca61f594f1732f58859bd0b8a0
Dinara <dinara@viaferrats.su>
2022-07-19 18:07:05 +0300

    second version

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,14 +9,18 @@ const Users = () => {
 
     };
     const renderPhrase = (number) => {
-      
+      const lastOne = Number(number.toString().slice(-1));
+      if (number > 4 && number < 15) return "человек тусанет";
+      if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+      if (lastOne === 1) return "человек тусанет";
+      return "человек тусанет";  
 
     };
   console.log(users);
   const renderQualities = (qualities) => {
     return qualities.map((qualitie) => {
       return (
-        <span key = {qualitie._id} className = {`badge bg-${qualitie.color} m-1`}>
+        <span key = {qualitie._id} className={`badge bg-${qualitie.color} m-1`}>
           {qualitie.name}
         </span> 
       )
@@ -29,7 +33,7 @@ const Users = () => {
         <td>{renderQualities(item.qualities)}</td>
         <td>{item.profession.name}</td>
         <td>{item.completedMeetings}</td>
-        <td>{item.rate}</td>
+        <td>{item.rate} /5</td>
         <td>
           <button className='btn btn-danger' onClick={() => handleDelete(item._id)}>delete</button>
         </td>
@@ -41,8 +45,11 @@ const Users = () => {
     return (
         <>
         <h2>
-            <span className={'badge ' + (users.length > 0 ? 'bg-primary' : 'bg-danger')}>
-            {renderPhrase}
+            <span className={'badge ' + (users.length > 0 ? 'bg-primary' : 'bg-danger')}
+            >
+              {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
             </span>
         </h2>
         <table className='table'>
